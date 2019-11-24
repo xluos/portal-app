bbf252051923ec57e5b5e1d3ade729776214a073
徐帅武 <xusw@guahao.com>
2019-11-24 18:08:25 +0800

    fix: 删除测试数据

--- src/index.js
+++ src/index.js
@@ -4,28 +4,28 @@ import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 import FrameWork from './lib/framework'
-import { Component } from './utils/loadable'
-import { Icon } from 'antd'
+// import { Component } from './utils/loadable'
+// import { Icon } from 'antd'
 function getSubApp () {
   return new Promise(res => {
     setTimeout(() => {
       res({
         routes: [
-          {
-            name: '菜单3-sub',
-            path: '/cc-sub',
-            icon: 'up-square',
-            exact: true,
-            // disableAsideMenu: true,
-            // disableHeaderMenu: true,
-            component: Component(() => import(/* webpackChunkName: "c" */ './pages/c')),
-          },
-          {
-            name: 'Setting-sub',
-            path: '/setting-sub',
-            icon: (<Icon type="setting" />),
-            component: Component(() => import(/* webpackChunkName: "c" */ './pages/setting')),
-          }
+          // {
+          //   name: '菜单3-sub',
+          //   path: '/cc-sub',
+          //   icon: 'up-square',
+          //   exact: true,
+          //   // disableAsideMenu: true,
+          //   // disableHeaderMenu: true,
+          //   component: Component(() => import(/* webpackChunkName: "c" */ './pages/c')),
+          // },
+          // {
+          //   name: 'Setting-sub',
+          //   path: '/setting-sub',
+          //   icon: (<Icon type="setting" />),
+          //   component: Component(() => import(/* webpackChunkName: "c" */ './pages/setting')),
+          // }
         ]
       })
     }, 300);
